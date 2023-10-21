b7b0099eb15e4a48875520143eb2283375ea9891
joseniquen08 <j.armando0807@gmail.com>
2023-10-21 11:48:56 -0500

    inicio del proyecto

---
+++ src/main/java/pe/edu/utp/olimpiadas_aqp/OlimpiadasAqpApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.utp.olimpiadas_aqp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlimpiadasAqpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlimpiadasAqpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/utp/olimpiadas_aqp/OlimpiadasAqpApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.utp.olimpiadas_aqp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlimpiadasAqpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
