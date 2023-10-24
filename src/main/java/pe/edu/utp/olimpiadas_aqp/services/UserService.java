945a67d440f71c16ed6ae4cd73a78eb6b9a90c12
LuisHEA <u17200953@utp.edu.pe>
2023-10-23 19:52:12 -0500

    cambios

--- src/main/java/pe/edu/utp/olimpiadas_aqp/services/UserService.java
+++ src/main/java/pe/edu/utp/olimpiadas_aqp/services/UserService.java
@@ -3,24 +3,27 @@ package pe.edu.utp.olimpiadas_aqp.services;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import pe.edu.utp.olimpiadas_aqp.entities.UserClientEntity;
 import pe.edu.utp.olimpiadas_aqp.entities.UserEntity;
+import pe.edu.utp.olimpiadas_aqp.models.requests.UserClienteRequest;
 import pe.edu.utp.olimpiadas_aqp.models.requests.UserRequest;
-import pe.edu.utp.olimpiadas_aqp.models.responses.UserResponse;
-import pe.edu.utp.olimpiadas_aqp.repositories.UserRepository;
+import pe.edu.utp.olimpiadas_aqp.models.responses.UserClientResponse;
+import pe.edu.utp.olimpiadas_aqp.repositories.UserClientRepository;
 
 @Service
 public class UserService implements UserServiceInterface {
 
     @Autowired
-    UserRepository userRepository;
+    UserClientRepository userClientRepository;
 
     @Override
-    public UserResponse create(UserRequest userRequest) {
-        UserEntity userEntity = new UserEntity();
-        BeanUtils.copyProperties(userRequest, userEntity);
-        userRepository.save(userEntity);
-        UserResponse userToReturn = new UserResponse();
-        BeanUtils.copyProperties(userEntity, userToReturn);
-        return userToReturn;
+    public UserClientResponse create(UserClienteRequest userClientRequest) {
+        UserClientEntity userClientEntity = new UserClientEntity();
+        BeanUtils.copyProperties(userClientRequest, userClientEntity);
+        userClientRepository.save(userClientEntity);
+        UserClientResponse userClientToReturn= new UserClientResponse();
+        BeanUtils.copyProperties(userClientEntity, userClientToReturn);
+        return userClientToReturn;
     }
 }
--- src/main/java/pe/edu/utp/olimpiadas_aqp/services/UserServiceInterface.java
+++ src/main/java/pe/edu/utp/olimpiadas_aqp/services/UserServiceInterface.java
@@ -1,9 +1,10 @@
 package pe.edu.utp.olimpiadas_aqp.services;
 
 import pe.edu.utp.olimpiadas_aqp.models.requests.AuthRequest;
+import pe.edu.utp.olimpiadas_aqp.models.requests.UserClienteRequest;
 import pe.edu.utp.olimpiadas_aqp.models.requests.UserRequest;
-import pe.edu.utp.olimpiadas_aqp.models.responses.UserResponse;
+import pe.edu.utp.olimpiadas_aqp.models.responses.UserClientResponse;
 
 public interface UserServiceInterface {
-    UserResponse create(UserRequest userRequest);
+    UserClientResponse create(UserClienteRequest userClientRequest);
 }
